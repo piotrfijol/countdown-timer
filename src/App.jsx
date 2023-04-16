f453666601bc71672fa782e43518e1861f5d175f
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-16 21:05:06 +0200

    Extract footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import './App.css'
 import './style.scss'
+import { Footer } from "./components/footer";
 
 function App() {
 
   return (
     <div className="App">
       <div className="stars-background"></div>
-      <footer className='footer'>
-        <div className="footer__hills">
-        </div>
-        <div className="footer__icons">
-          <div className="footer__ icon facebook"></div>
-          <div className="footer__ icon pinterest"></div>
-          <div className="footer__ icon instagram"></div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.scss';
+
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className="footer__hills">
+      </div>
+      <div className="footer__icons">
+        <div className="footer__ icon facebook"></div>
+        <div className="footer__ icon pinterest"></div>
+        <div className="footer__ icon instagram"></div>
+      </div>
+    </footer>
+  )
+}
