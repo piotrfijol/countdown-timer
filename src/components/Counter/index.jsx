299bfe51104d7ee9adb5b983bb0e42f63e5fcab4
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-19 19:42:40 +0200

    Optimize counter component

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -2,62 +2,66 @@ import React, { useEffect, useState } from 'react'
 import { FlipCountdown } from "../FlipCountdown";
 import "./counter.scss"
 
-export const Counter = () => {
-    let [seconds, setSeconds] = useState(0);
-    let [minutes, setMinutes] = useState(0);
-    let [hours, setHours]     = useState(0);
-    let [days, setDays]       = useState(0);
-    let currentDate = new Date();
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    const FINAL_DATE = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+let currentDate = new Date();
+currentDate.setMonth(currentDate.getMonth() + 1);
 
+const FINAL_DATE = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+const getTimeLeft = () => {
+
+    let timeLeft = FINAL_DATE.getTime() - Date.now();
+    let timestampInSeconds = parseInt(timeLeft/1000);
+
+    return {
+        seconds: timestampInSeconds % 60,
+        minutes: parseInt((timestampInSeconds % (60*60)) / 60),
+        hours: parseInt(timestampInSeconds % (60*60*24) / (60*60)),
+        days: parseInt(timestampInSeconds / (60*60*24)),
+    };
+};
+
+export const Counter = () => {
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft());
     
     useEffect(() => {
         
         let timeoutID;
-        let timeLeft = FINAL_DATE.getTime() - Date.now();
-        if(timeLeft > 0) {
-            let timestampInSeconds = parseInt(timeLeft/1000);
-
-            timeoutID = setInterval(() => {
-                setSeconds(timestampInSeconds % 60);
-                setMinutes(parseInt((timestampInSeconds % (60*60)) / 60));
-                setHours(parseInt(timestampInSeconds % (60*60*24) / (60*60)));
-                setDays(parseInt(timestampInSeconds / (60*60*24)));
+        const timeLeftObj = getTimeLeft();
+        if(Object.values(timeLeftObj).some(val => val !== 0)) {
+            timeoutID = setTimeout(() => {
+                setTimeLeft(timeLeftObj);
             }, 1000);
-
-        }   
-
-        return () => {
-            if(timeoutID)
-                clearInterval(timeoutID);
         }
 
-    }, [seconds, minutes, hours, days]);
+        return () => {
+            clearTimeout(timeoutID)
+        };
+    }, [timeLeft]);
+
 
   return (
         <div className="counter">
             <div className="digits-col">
                 <FlipCountdown 
-                    current={days}
+                    current={timeLeft.days}
                 />
                 <p className="label">DAYS</p>
             </div>
             <div className="digits-col">
                 <FlipCountdown 
-                    current={hours}
+                    current={timeLeft.hours}
                 />
                 <p className="label">HOURS</p>
             </div>
             <div className="digits-col">
                 <FlipCountdown 
-                    current={minutes}
+                    current={timeLeft.minutes}
                 />
                 <p className="label">MINUTES</p>
             </div>
             <div className="digits-col">
                 <FlipCountdown 
-                    current={seconds}
+                    current={timeLeft.seconds}
                 />
                 <p className="label">SECONDS</p>
             </div>
