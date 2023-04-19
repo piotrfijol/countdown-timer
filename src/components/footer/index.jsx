5f26d6baa3dc519aead7b212734e1e43bd7fdabb
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-19 11:46:50 +0200

    Add footer icons

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './footer.scss';
+import FacebookIcon from '@assets/icon-facebook.svg';
+import PinterestIcon from '@assets/icon-pinterest.svg';
+import InstagramIcon from '@assets/icon-instagram.svg';
 
 export const Footer = () => {
   return (
@@ -7,9 +10,21 @@ export const Footer = () => {
       <div className="footer__hills">
       </div>
       <div className="footer__icons">
-        <div className="footer__ icon facebook"></div>
-        <div className="footer__ icon pinterest"></div>
-        <div className="footer__ icon instagram"></div>
+        <div className="footer__ icon facebook">
+          <a href="#">
+            <img src={FacebookIcon} alt="" />
+          </a>
+        </div>
+        <div className="footer__ icon pinterest">
+          <a href="#">
+            <img src={PinterestIcon} alt="" />
+          </a>
+        </div>
+        <div className="footer__ icon instagram">
+          <a href="#">
+            <img src={InstagramIcon} alt="" />
+          </a>
+        </div>
       </div>
     </footer>
   )
